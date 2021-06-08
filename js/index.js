06cedf2efb9feac6628cafc7b42007f6fee204b2
Marsel Gabbasov <marsel13_98@mail.ru>
2021-06-08 14:02:02 +0300

    2 commit

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 function sliderMain() {
-    var swiper = new Swiper('.main .swiper-container', {
-        speed: 800,
+    var swiper = new Swiper('.main--index .swiper-container', {
+        speed: 200,
         slidesPerView: 3,
         spaceBetween: 0,
         loop: true,
 
         navigation: {
-            nextEl: '.main .swiper-button-next',
-            prevEl: '.main .swiper-button-prev',
+            nextEl: '.main--index .swiper-button-next',
+            prevEl: '.main--index .swiper-button-prev',
         },
         pagination: {
-            el: ".main .swiper-pagination",
+            el: ".main--index .swiper-pagination",
             // dynamicBullets: true,
         },
         breakpoints: {
@@ -32,14 +32,48 @@ function sliderMain() {
 }
 
 
+function sliderMainService() {
+    var swiper = new Swiper('.main--service .swiper-container', {
+        speed: 200,
+        slidesPerView: 3,
+        spaceBetween: 0,
+        loop: true,
 
+        navigation: {
+            nextEl: '.main--service .swiper-button-next',
+            prevEl: '.main--service .swiper-button-prev',
+        },
+        pagination: {
+            el: ".main--service .swiper-pagination",
+            // dynamicBullets: true,
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 5,
+            },
+            768: {
+                slidesPerView: 1,
+                spaceBetween: 15,
+            },
+
+            992: {
+                slidesPerView: 4,
+                spaceBetween: 20,
+            },
+        }
+    })
+}
 $(document).ready(function() {
     if ($(window).width() < 992) {
         sliderMain()
+        sliderMainService()
     }
 
     $(".header__burger").click(function() {
         $(this).toggleClass("header__burger--active");
         $(".header__nav").slideToggle();
     })
+
+
 })
